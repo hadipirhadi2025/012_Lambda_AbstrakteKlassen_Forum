11562d7cf1d22405b2b788d3a2bbfc2047cb36aa
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-07-03 11:37:22 +0200

    Add Rechner functional interface to demonstrate lambda expressions with multiple parameters

---
+++ src/de/schlaukopf/organisation/Rechner.java
@@ -0,0 +1,13 @@
+package de.schlaukopf.organisation;
+
+/**
+ * <pre>
+ *     Ein Funktionales Interface, demonstierert , wie
+ *     Lambda Ausdrücke aussehen, wenn die abstrakte Methode mehr
+ *     als einen Parameter hat
+ * </pre>
+ *
+ */
+public interface Rechner {
+    double berechnen(double d1, double d2);
+}
